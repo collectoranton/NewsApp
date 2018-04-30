ba5dff22156f7b2cbb5a515b97fce97fb9df1de1
collectoranton <anton.lilja@collectorbank.se>
2018-04-30 22:04:56 +0200

    First commit

---
+++ NewsApp/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsApp
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string Intro { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
---
+++ NewsApp/NewsAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewsApp
+{
+    public class NewsAppContext : DbContext
+    {
+        public DbSet<News> News { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename = NewsDb.db");
+        }
+    }
+}
